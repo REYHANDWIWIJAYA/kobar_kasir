38df0f6bc9b7a4594f900fffc5b3cc7e89e2e023
reyhan <105841112623@gmail.com>
2026-09-25 23:14:23 +0800

    fix: update URL Supabase xofbnrepyqasucbnwnjv

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://xofbnrepyqasucbnwjv.supabase.co';
+const SUPABASE_URL = 'https://xofbnrepyqasucbnwnjv.supabase.co';
 const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvZmJucmVweXFhc3VjYm53bmp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzNDE1NzEsImV4cCI6MjEwNTkxNzU3MX0.9cJprwdXMxIcwo-r7_BhfRNFsEQWqfFH0jx7ZnuJpVI';
 
